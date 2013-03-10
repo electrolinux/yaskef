1f814e07799de005436a816ae480469df89e55c3
Didier Belot <electrolinux@gmail.com>
2013-03-10 12:42:57 +0100

    bootstrap/min.js loaded from prod.php

--- src/SKE/Composer/Script.php
+++ src/SKE/Composer/Script.php
@@ -10,11 +10,6 @@ class Script
         chmod('resources/log', 0777);
         chmod('web/assets', 0777);
         chmod('console', 0500);
-		if (PHP_OS == 'WINNT') {
-			// no symlink !!
-			unlink('resources/assets/js/bootstrap.min.js');
-			copy('vendor/twitter/bootstrap/docs/assets/js/bootstrap.min.js','resources/assets/js/bootstrap.min.js');
-		}
         exec('php console assetic:dump');
     }
 }
