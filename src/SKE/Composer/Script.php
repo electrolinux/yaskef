3ecf1b123e04767e203609cf6ad52c0723dddfbc
Didier Belot <electrolinux@gmail.com>
2013-03-11 01:56:49 +0100

    Ready for rc1 (the good one, this time)

--- src/SKE/Composer/Script.php
+++ src/SKE/Composer/Script.php
@@ -12,7 +12,7 @@ class Script
         chmod('console', 0500);
         copy('resources/config/db_config.php.dist','resources/config/db_config.php');
         chmod('resources/config/db_config.php',0666);
-        chmod('ressources/db',0777);
+        chmod('resources/db',0777);
         exec('php console assetic:dump');
     }
 }
